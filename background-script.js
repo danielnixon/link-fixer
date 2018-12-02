c377239ff33081db8c30b5da124f1c1fa01d1676
Daniel Nixon <dan.nixon@gmail.com>
2018-12-02 16:46:37 +1100

    Simulate native tab order behaviour. Fix #3

--- background-script.js
+++ background-script.js
@@ -1,3 +1,20 @@
+const dropWhile = (f, xs) => xs.length ? dropWhileNotEmpty(f, xs) : [];
+const dropWhileNotEmpty = (f, [x, ...xs]) =>  f(x) ? dropWhile(f, xs) : [x, ...xs];
+const takeWhile = (f, xs) => xs.length ? takeWhileNotEmpty(f, xs) : [];
+const takeWhileNotEmpty = (f, [x, ...xs]) =>  f(x) ? [x, ...takeWhile(f, xs)] : [];
+const last = (xs) => xs.slice(-1)[0];
+
+const calculateNewTabIndex = function(senderTab, tabs) {
+  if (senderTab) {
+    var tabsAfterSenderTab = dropWhile(tab => tab.index <= senderTab.index, tabs);
+    var tabsOpenedBySenderTab = takeWhile(tab => tab.openerTabId === senderTab.id, tabsAfterSenderTab);
+    var lastTabOpenedBySenderTab = last(tabsOpenedBySenderTab);
+    return lastTabOpenedBySenderTab ? lastTabOpenedBySenderTab.index + 1 : undefined;
+  } else {
+    return undefined;
+  }
+};
+
 chrome.runtime.getPlatformInfo(function(info) {
   const isMac = info.os === "mac";
 
@@ -11,11 +28,16 @@ chrome.runtime.getPlatformInfo(function(info) {
       const shouldOpenTab = !(isMac && message.ctrlKey);
 
       if (shouldOpenTab) {
-        chrome.tabs.create({
-          url: message.url,
-          active: false,
-          index: sender.tab ? sender.tab.index + 1 : undefined
-        });
+        chrome.tabs.query({
+          windowId: sender.tab. windowId
+        }, function(tabs) {
+          chrome.tabs.create({
+            url: message.url,
+            active: false,
+            openerTabId: sender.tab.id,
+            index: calculateNewTabIndex(sender.tab, tabs)
+          });
+        })
       }
     }
   });
